9af1a3b0ef9f3ef5faf5e8c492c87e01d9b8d29d
Caleb Thompson <calebthompson89@gmail.com>
2012-06-05 20:17:23 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Video::Application.config.secret_token = '9ed3b318db5090e51e90e806b856a2e35b35cef20456c2a4198c9e76eb923c14aaada8510f89044021456465bdc1cdabd9ae80189763b745fb76f3db8f088222'
